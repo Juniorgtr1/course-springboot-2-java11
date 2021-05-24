b1e92e03a69b58a8f6c92610e5cc5068791480c8
Junior Sousa <juniorgtr1@gmail.com>
2021-05-24 16:25:57 -0300

    H2 database, test profile, JPA

--- src/main/java/com/juniorsousa/course/entities/User.java
+++ src/main/java/com/juniorsousa/course/entities/User.java
@@ -2,21 +2,29 @@ package com.juniorsousa.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	private String emial;
+	private String email;
 	private String phone;
 	private String password;
 	
 	public User() {}
 
-	public User(Long id, String name, String emial, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		this.id = id;
 		this.name = name;
-		this.emial = emial;
+		this.email = email;
 		this.phone = phone;
 		this.password = password;
 	}
@@ -37,12 +45,12 @@ public class User implements Serializable{
 		this.name = name;
 	}
 
-	public String getEmial() {
-		return emial;
+	public String getEmail() {
+		return email;
 	}
 
-	public void setEmial(String emial) {
-		this.emial = emial;
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getPhone() {
