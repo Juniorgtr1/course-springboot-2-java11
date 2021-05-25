77e0d63d2a2d2ce8e7960f3b330178272b9b8512
Junior Sousa <juniorgtr1@gmail.com>
2021-05-25 11:13:56 -0300

    Order, Instant, ISO 8601 - part12

---
+++ src/main/java/com/juniorsousa/course/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.juniorsousa.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.juniorsousa.course.entities.Order;
+import com.juniorsousa.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
