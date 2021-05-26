82d7d32e19c10b8ce505ef967f1f3f695b1838cd
Junior Sousa <juniorgtr1@gmail.com>
2021-05-26 08:57:13 -0300

    Category

---
+++ src/main/java/com/juniorsousa/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.juniorsousa.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.juniorsousa.course.entities.Category;
+import com.juniorsousa.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
